c10516583630183c1a62810c2c29cd50bbdb7872
serenie22 <41089378+serenie22@users.noreply.github.com>
2020-03-04 23:14:49 -0500

    Create Cell.java

---
+++ Cell.java
@@ -0,0 +1,6 @@
+public interface Cell
+{
+	public String abbreviatedCellText(); // text for spreadsheet cell display, must be exactly length 10
+	public String fullCellText(); // text for individual cell inspection, not truncated or padded
+	public int compareTo (Object cell);
+}
